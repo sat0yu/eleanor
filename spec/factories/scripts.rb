54fa6979964d5476251898d2f0d58c1f8d6dd6c1
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2018-01-02 11:02:10 +0900

    added two columns "length" and "description" to Script

--- spec/factories/scripts.rb
+++ spec/factories/scripts.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :script do
-    title { Faker::StarWars.quote }
-    body  { Faker::StarWars.quote }
+    title       { Faker::StarWars.quote }
+    description { Faker::StarWars.quote }
+    body        { Faker::StarWars.quote }
+    length      { body&.length || 0 }
   end
 end
--- spec/models/script_spec.rb
+++ spec/models/script_spec.rb
@@ -23,6 +23,28 @@ RSpec.describe Script, type: :model do
     end
   end
 
+  describe '#description' do
+    context 'normal cases' do
+      context 'when larger than 1B and smaller than 128KB' do
+        it{ expect( build(:script, description: 'description').validate ).to be_truthy }
+      end
+    end
+
+    context 'error cases' do
+      context 'when given nil' do
+        it{ expect( build(:script, description: nil).validate ).to be_falsey }
+      end
+
+      context 'when given empty' do
+        it{ expect( build(:script, description: '').validate ).to be_falsey }
+      end
+
+      context 'when given larger than 128KB' do
+        it{ expect( build(:script, description: 'a' + 'a' * 128.kilobytes).validate ).to be_falsey }
+      end
+    end
+  end
+
   describe '#body' do
     context 'normal cases' do
       context 'when larger than 1B and smaller than 4MB' do
@@ -41,6 +63,28 @@ RSpec.describe Script, type: :model do
 
       context 'when given larger than 4MB' do
         it{ expect( build(:script, body: 'a' + 'a' * 4.megabytes).validate ).to be_falsey }
+      end
+    end
+  end
+
+  describe '#length' do
+    context 'normal cases' do
+      context 'when greater than 0' do
+        it{ expect( build(:script, length: 1).validate ).to be_truthy }
+      end
+
+      context 'when given 0' do
+        it{ expect( build(:script, length: 0).validate ).to be_truthy }
+      end
+    end
+
+    context 'error cases' do
+      context 'when given nil' do
+        it{ expect( build(:script, length: nil).validate ).to be_falsey }
+      end
+
+      context 'when given non-positive integer' do
+        it{ expect( build(:script, length: -1).validate ).to be_falsey }
       end
     end
   end
